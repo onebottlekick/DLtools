02b7acb50eed03177bdfa7d2295064392da62b1b
onebottlekick <gksqudcks97@gmail.com>
2021-08-19 00:45:45 +0900

    functions

---
+++ functions/__init__.py
@@ -0,0 +1,7 @@
+from .activation import step_function
+from .activation import sigmoid
+from .activation import relu
+from .activation import softmax
+from .plot import plot_activation_func
+
+__all__ = ['step_function', 'sigmoid', 'relu', 'softmax', 'plot_activation_func']
