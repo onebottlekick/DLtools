bd3dc3f259861d169a5833df99182e2fafe22552
onebottlekick <gksqudcks97@gmail.com>
2021-08-19 23:41:21 +0900

    gradient descent

--- functions/gradient.py
+++ functions/gradient.py
@@ -1,4 +1,7 @@
-from diff import numerical_gradient
+from .diff import numerical_gradient
 
-def gradient_descent(func, learning_rate=0.01, step_num=100):
-    
+def gradient_descent(func, x, learning_rate=0.01, step_num=100):
+    for i in range(step_num):
+        grad = numerical_gradient(func, x)
+        x -= learning_rate * grad
+    return x
