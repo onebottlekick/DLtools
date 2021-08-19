9c21c287445a7b64a5beeb9f354ab7ead5b882ef
onebottlekick <gksqudcks97@gmail.com>
2021-08-19 18:13:16 +0900

    updated

--- neural_net/network.py
+++ neural_net/network.py
@@ -20,7 +20,7 @@ def activation(func_name : str, a : np.ndarray):
         
 """
 Usage:
-net = NeuralNet()
+model = NeuralNet()
 parameters = {
     'W1' : np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
     'W2' : np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
@@ -29,20 +29,22 @@ parameters = {
     'b2' : np.array([0.1, 0.2]),
     'b3' : np.array([0.1, 0.2])
     }
-net.add(parameters=parameters)
-net.forward(np.array([1.0, 0.5]), ['sigmoid', 'sigmoid', 'identity'])
+model.add(parameters=parameters)
+model.fit(np.array([1.0, 0.5]), ['sigmoid', 'sigmoid', 'identity'])
+y_pred = model.predict()
 """
 class NeuralNet:
     def __init__(self):
         self.layers = {}
         self.weights = []
         self.biases = []
+        self.y_pred = None
     
     def add(self, parameters : Dict[str, np.ndarray]):
         for name, value in parameters.items():
             self.layers[name] = value
 
-    def forward(self, x : np.ndarray, act_func : List[str]) -> np.ndarray:
+    def fit(self, x : np.ndarray, act_func : List[str]) -> np.ndarray:
         for layer in self.layers.items():
             try:
                 if layer[0][0][0] == 'W':
@@ -50,13 +52,19 @@ class NeuralNet:
                 if layer[0][0][0] == 'b':
                     self.biases.append(layer)
             except Exception as e:
-                print(e)
+                print(e, "input example : {'W+idx or b+idx' : np.ndarray}")
+        try:
+            self.weights = sorted(self.weights, key=lambda x:x[0][-1])
+            self.biases = sorted(self.biases, key=lambda x:x[0][-1])
+            a = np.dot(x, self.weights[0][1]) + self.biases[0][1]
+            z = activation(act_func[0], a)
+            for i in range(1, len(self.weights) - 1):
+                a = np.dot(z, self.weights[i][1]) + self.biases[i][1]
+                z = activation(act_func[i], a)
+            a = np.dot(z, self.weights[-1][1]) + self.biases[-1][1]
+            self.y_pred = activation(act_func[-1], a)
+        except Exception as e:
+            print(e)
 
-        a = np.dot(x, self.weights[0][1]) + self.biases[0][1]
-        z = activation(act_func[0], a)
-        for i in range(1, len(self.weights) - 1):
-            a = np.dot(z, self.weights[i][1]) + self.biases[i][1]
-            z = activation(act_func[i], a)
-        a = np.dot(z, self.weights[-1][1]) + self.biases[-1][1]
-        y = activation(act_func[-1], a)
-        return y
+    def predict(self):
+        return self.y_pred
