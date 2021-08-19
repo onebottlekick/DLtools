7ac7bedc8b9fc67c51296e115ede44a02d396503
onebottlekick <gksqudcks97@gmail.com>
2021-08-19 20:26:47 +0900

    .

--- functions/__init__.py
+++ functions/__init__.py
@@ -6,5 +6,9 @@ from .activation import identity
 from .plot import plot_activation_func
 from .plot import plot_img
 from .metrics import accuracy_score
+from .diff import numerical_gradient
+from .loss import cross_entropy_error
+from .loss import sum_sqares_error
 
-__all__ = ['step_function', 'sigmoid', 'relu', 'softmax','identity',  'plot_activation_func', 'plot_img', 'accuracy_score']
+__all__ = ['step_function', 'sigmoid', 'relu', 'softmax','identity',  'plot_activation_func', 'plot_img',
+        'accuracy_score', 'numerical_gradient', 'cross_entropy_error', 'sum_sqares_error']
