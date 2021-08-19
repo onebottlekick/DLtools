93eff3aac34787b7e9fb63861703d93ca59fdf37
onebottlekick <gksqudcks97@gmail.com>
2021-08-20 00:22:37 +0900

    layer name -> layer index

--- layers/layer.py
+++ layers/layer.py
@@ -2,23 +2,22 @@ import numpy as np
 
 
 class Layer:
-    def __init__(self,  node_size : int, input_size : int, names=None, activation : str=None):
-        self.names = names
+    def __init__(self, idx : int, node_size : int, input_size : int, activation : str=None):
+        self.idx = idx
         self.node_size = node_size
         self.input_size = input_size
         self.activation = activation
-        self.layer_list = None
+        self.layer_dict = None
     
     def layer(self):
         self.layer_dict = {
-            self.names[0] : np.random.randn(self.node_size).reshape(self.input_size, -1),
-            self.names[1] : np.random.randn(int(self.node_size/self.input_size))
+            "W" + str(self.idx) : np.random.randn(self.node_size).reshape(self.input_size, -1),
+            "b" + str(self.idx) : np.random.randn(int(self.node_size/self.input_size))
         }
-
         return self.layer_dict
 
     def output_size(self):
-        return self.layer_dict[self.names[0]][1].shape[-1]
+        return self.layer_dict[self.layer_dict['W' + self.idx]][1].shape[-1]
 
     def activation_function(self):
         if self.activation:
