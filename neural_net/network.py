12242f34b4ede1c00bfe267f0e1c8a48f7a4d91a
onebottlekick <gksqudcks97@gmail.com>
2021-08-19 17:01:50 +0900

    added activation function choice parameter

--- neural_net/network.py
+++ neural_net/network.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from functions.activation import sigmoid, identity
-from typing import Dict
+from functions.activation import sigmoid, identity, softmax, relu
+from typing import Dict, List
 
 
 """
@@ -12,6 +12,20 @@ net.add(np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]), np.array([0.1, 0.2]))
 net.add(np.array([[0.1, 0.3], [0.2, 0.4]]), np.array([0.1, 0.2]))
 print(net.forward(np.array([1.0, 0.5])))
 """
+
+def activation(func_name : str, a : np.ndarray):
+    func_list = ['sigmoid', 'identity', 'softmax', 'relu']
+    if func_name not in func_list:
+        raise ValueError('not a valid function name')
+    if func_name == 'sigmoid':
+        return sigmoid(a)
+    elif func_name == 'identity':
+        return identity(a)
+    elif func_name == 'softmax':
+        return softmax(a)
+    elif func_name == 'relu':
+        return relu(a)
+
 class NeuralNet:
     def __init__(self):
         self.layers = {}
@@ -22,7 +36,7 @@ class NeuralNet:
         for name, value in parameters.items():
             self.layers[name] = value
 
-    def forward(self, x : np.ndarray) -> np.ndarray:
+    def forward(self, x : np.ndarray, act_func : List[str]) -> np.ndarray:
         for layer in self.layers.items():
             try:
                 if layer[0][0][0] == 'W':
@@ -33,10 +47,10 @@ class NeuralNet:
                 print(e)
 
         a = np.dot(x, self.weights[0][1]) + self.biases[0][1]
-        z = sigmoid(a)
+        z = activation(act_func[0], a)
         for i in range(1, len(self.weights) - 1):
             a = np.dot(z, self.weights[i][1]) + self.biases[i][1]
-            z = sigmoid(a)
+            z = activation(act_func[i], a)
         a = np.dot(z, self.weights[-1][1]) + self.biases[-1][1]
-        y = identity(a)
+        y = activation(act_func[-1], a)
         return y
