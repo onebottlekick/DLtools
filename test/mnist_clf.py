13937f7516223a77969591cabb541561a8cead0d
onebottlekick <gksqudcks97@gmail.com>
2021-08-19 18:28:20 +0900

    .

--- test/mnist_clf.py
+++ test/mnist_clf.py
@@ -5,11 +5,12 @@ from neural_net import NeuralNet
 from functions import accuracy_score
 
 
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 X_train = X_train.reshape(60000, -1)/255.0
 X_test = X_test.reshape(10000, -1)/255.0
 
-with open('sample_weight.pkl', 'rb') as f:
+with open('assets/sample_weight.pkl', 'rb') as f:
     sample = pickle.load(f)
 
 
